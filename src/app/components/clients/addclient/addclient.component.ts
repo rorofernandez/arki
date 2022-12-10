f46bf43c0f3005696348d18cc5adbc7c6b9e128f
rorofernandez <35464603+rorofernandez@users.noreply.github.com>
2022-12-10 13:31:19 -0800

    Edit button on Clients Page now pulls the data from the data file

--- src/app/components/clients/addclient/addclient.component.ts
+++ src/app/components/clients/addclient/addclient.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -14,10 +14,43 @@ export class AddclientComponent implements OnInit {
   loader: boolean = false;
   tempFile: any;
 
+  @Input() public clientInfo:any;
+  @Output()
+  public closeModel: EventEmitter<void> = new EventEmitter<void>();
 
   constructor( private modalService: NgbModal, private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    if(this.clientInfo) {
+      this.initialForm(this.clientInfo);
+    } else{
+      this.initialForm();
+    }
+   
   }
 
+  initialForm(clientObj: any = null) {
+    if (clientObj === null) {
+      this.addClientForm = this.fb.group({
+        firstName: ["", Validators.required],
+        lastName: ["", Validators.required],
+        email: ["", Validators.required],
+        contact: ["", Validators.required],
+        clientId: [null],
+        addedOn: []
+      });
+    } else {
+      this.addClientForm = this.fb.group({
+        firstName: [clientObj.firstName, Validators.required],
+        lastName: [clientObj.lastName, Validators.required],
+        email: [clientObj.email, Validators.required],
+        contact: [clientObj.contact, Validators.required],
+        clientId: [clientObj.clientId]
+      });
+    }
+  }
+
+  close() {
+    this.closeModel.emit();
+  }
 }
