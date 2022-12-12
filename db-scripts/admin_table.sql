6daf90f477dece175ac080169b1c61571df20624
rorofernandez <35464603+rorofernandez@users.noreply.github.com>
2022-12-11 19:11:36 -0800

    added mysql scripts under  db-scripts folder

---
+++ db-scripts/admin_table.sql
@@ -0,0 +1,12 @@
+mysql > CREATE TABLE ADMIN (
+  adminID       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  firstName     VARCHAR(500) NOT NULL,
+  lastName      VARCHAR(500) NOT NULL,
+  email         VARCHAR(500) NOT NULL,
+  password      VARCHAR(500) NOT NULL
+);
+
+
+mysql> SHOW TABLES;
+
+mysql> DESC MEETINGS;
---
+++ db-scripts/meetings_table.sql
@@ -0,0 +1,12 @@
+mysql > CREATE TABLE MEETINGS (
+  meetingID       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  topic       VARCHAR(500) NOT NULL,
+  date        VARCHAR(500) NOT NULL,
+  time           VARCHAR(500) NOT NULL,
+  participants        VARCHAR(500) NOT NULL
+);
+
+
+mysql> SHOW TABLES;
+
+mysql> DESC CLIENTS;
